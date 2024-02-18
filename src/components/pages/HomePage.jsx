70488b303b10556e0690578793b2207204857cdd
Govind S Kumar <govindsajeev2001@gmail.com>
2024-02-18 19:31:14 +0530

    first commit

---
+++ src/components/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from '../Home'
+import Items from '../Items'
+
+
+const HomePage = () => {
+  return (
+    <div className='homepage'>
+      <Home/>
+      <Items/>
+    </div>
+  )
+}
+
+export default HomePage
